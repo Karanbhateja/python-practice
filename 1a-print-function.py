9f587fce01f6c0a131082b261c61206f8af02cba
Mr Robot <mrrobot@localhost.localdomain>
2022-09-23 19:16:11 +0530

    Day 1

---
+++ 1a-print-function.py
@@ -0,0 +1,4 @@
+print("hello '1' world")
+print('hello "2" world')
+
+# You can use single quotes inside double quotes and double inside single but you cannot use single inside single and double inside double.
