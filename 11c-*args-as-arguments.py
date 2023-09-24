2205ede0fedd13ad5e70982b8ab69985bb931d3c
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2023-09-24 17:13:57 +0530

    Day 16

---
+++ 11c-*args-as-arguments.py
@@ -0,0 +1,12 @@
+# Suppose we have to pass list as a argument when the function has *args as a parameter then, 
+def add(*args):
+  for i in args:
+    result += i
+  return result
+
+# List -
+l = [1,2,3,4] # Now we want to use this list as argument to the multiply function
+
+print(multiply(*l))
+
+# We can use * to unpack the list and pass its elements to the function
