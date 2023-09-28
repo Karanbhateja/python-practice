4688f745483dc8f8f2f9cc44fc14506250ab7746
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2023-09-28 23:53:15 +0530

    Day 18

---
+++ 13a-Enumerate-Function.py
@@ -0,0 +1,8 @@
+# To track the position in an iterable loop we can do it with for loop as follows - 
+names = ['abc', 'abcd', 'abcde']
+pos = 0 
+for i in names:
+  print(f"{pos}---> {i}")
+  pos += 1
+
+# Continue...............
